24d1b79e5f4d036f099fd943e136ec21b0c6bd2d
Susritha Kopparapu <skoppara@bu.edu>
2021-02-09 18:56:35 -0500

    Adding new code to download.Wasting_data.R

--- 01_download.Wasting_Data.R
+++ 01_download.Wasting_Data.R
@@ -1,3 +1,7 @@
 install.packages("readr")
 library(readr)
 data <- read_csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
+
+library(ggplot2)
+boxplot(data$`Average WD (%)`~Transect,data=data, main="not sure what to put here",
+        xlab="Transect", ylab="Average wasting disease %")
